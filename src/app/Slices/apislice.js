697971c291ed9363eabca8219d23afe9f3555e4c
rayane <laggounrayan00@gmail.com>
2024-05-19 20:39:52 +0100

    render commit

--- src/app/Slices/apislice.js
+++ src/app/Slices/apislice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://jurixproback.onrender.com" }),
   endpoints: (builder) => ({
     signUp: builder.mutation({
       query: (data) => ({
